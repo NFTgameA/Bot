e9e244c7c2baeb3f8874219aaa4c5c9160d502ec
NFTGameA <joehirest72@gmail.com>
2023-11-29 12:19:38 +0700

    fix

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 // Import packages
 const express = require("express");
-const home = require("./routes/home");
 
-// Middlewares
 const app = express();
 app.use(express.json());
+const home = require("./routes/home");
+
+const cors = require("cors");
+
+app.use(cors());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => {
+  res.json("Meme Bot");
+});
+
+// Middlewares
+
 
 // Routes
 app.use("/home", home);
 
 // connection
 const port = process.env.PORT || 9001;
+
 app.listen(port, () => console.log(`Listening to port ${port}`));
